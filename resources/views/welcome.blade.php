89e3651242d9d8d3042bc1e559d121d175291107
Prestes98 <davidupts@gmail.com>
2024-04-02 20:08:23 -0300

    primeiro commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,16 @@
 @endphp
 @include('includes.head')
 
-<body class="antialiased bg-secondary bg-gradient max-w d-flex justify-content-center align-items-center" style="height: 100vh">
-    <div class="card border-warning d-flex white shadow-lg " style="width: 50%; height:50%">'
-       <div class="d-flex justify-content-end">
-            <img src="assets/img/logo.png" width="100">
+<body class="antialiased bg-secondary bg-white max-w d-flex justify-content-center align-items-center" style="height: 100vh">
+    <div class="card border-warning d-flex white shadow-lg w-100" style="max-width: 600px; height: 500px">
+       <div class="d-flex justify-content-around p-5 hstack gap-5">
+        <h2 class="p-1 border border-dark p-1 bg-warning rounded-2 ">Biblioteca Legal</h2>
+        <img class="float-right" src="assets/img/bibliotecalegal2.png" width="180">
        </div>
        <div class="d-flex justify-content-center align-items-center">
-       <form class="w-50 mt-1" autocomplete="off">
+       <form class="w-50 mt-0" autocomplete="off">
         <div>
-            <label class="form-label" for="usuario">Usuario ou Email</label>
+            <label class="form-label" for="usuario">Usuário ou Email</label>
             <input class="form-control border-warning" type="text" name="usuario" placeholder="Digite seu usuario ou email" autocomplete="new-password">
         </div>
 
@@ -22,7 +23,7 @@
             <input class="form-control border-warning" type="password" name="senha" placeholder="Digite sua senha" autocomplete="new-password">
         </div>
         <div class="d-flex justify-content-center">
-        <button class="btn btn-primary mt-3">Entrar</button>
+        <button class="btn btn-primary mt-5">Entrar</button>
         </div>
         </form>        
        </div>
